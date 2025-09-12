897a50f49b5f86b268d3fb92e1a3506dd74c154e
Luis Velasco <luisvelasco6541@gmail.com>
2025-09-12 13:38:54 -0500

    change the header

--- components/header-dev.tsx
+++ components/header-dev.tsx
@@ -3,10 +3,10 @@ import { Github, Twitter, Linkedin } from "lucide-react";
 export default function HeaderDev() {
   return (
     <header className="flex justify-between items-center gap-2 py-8 text-gray-800">
-      <h1 className="text-xl font-semibold ">TrendsAI</h1>
+      <h1 className="text-xl font-semibold ">LuisRoftl</h1>
       <div className="flex gap-4 text-zinc-500">
         <a
-          href="https://github.com/luisdev"
+          href="https://github.com/LuisRoft/"
           target="_blank"
           rel="noopener noreferrer"
           className="hover:text-gray-800 transition"
@@ -14,7 +14,7 @@ export default function HeaderDev() {
           <Github />
         </a>
         <a
-          href="https://twitter.com/luisdev"
+          href="https://x.com/luisroftl"
           target="_blank"
           rel="noopener noreferrer"
           className="hover:text-gray-800 transition"
@@ -22,7 +22,7 @@ export default function HeaderDev() {
           <Twitter />
         </a>
         <a
-          href="https://linkedin.com/in/luisdev"
+          href="https://www.linkedin.com/in/luisvelasco27/"
           target="_blank"
           rel="noopener noreferrer"
           className="hover:text-gray-800 transition"
